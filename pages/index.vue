58e98012d53f367d65728ff831a3772640b80969
makial24 <you@example.com>
2022-03-30 21:41:08 +0800

    2nd Update

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to SpeedMaths Academy!</h1>
-      <p class="mt-5">Makes Your Child faster Than Calculator</p>
+      <h1 class="text-5xl">Welcome to SpeedMaths Super Academy!</h1>
+      <p class="mt-5">Makes Your Child faster Than Computer</p>
     </div>
   </div>
 </template>
