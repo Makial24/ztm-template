57424dc0c904034eb0ee3217bfa883b92b52baba
makial24 <you@example.com>
2022-03-30 20:59:46 +0800

    Updated Title

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+      <h1 class="text-5xl">Welcome to SpeedMaths Academy!</h1>
+      <p class="mt-5">Makes Your Child faster Than Calculator</p>
     </div>
   </div>
 </template>
