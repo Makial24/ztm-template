deeeee402e8561c92a32797f5d3ade1fd7a6486e
makial24 <you@example.com>
2022-04-03 23:32:00 +0800

    Project update 1

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,27 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
-    <div class="m-auto">
-      <h1 class="text-5xl">Welcome to SpeedMaths Super Academy!</h1>
-      <p class="mt-5">Makes Your Child faster Than Computer</p>
+  <div id='header' class="bg-[url('/hero-bg.jpg')] px-20">
+    <div id='menu' class="grid grid-cols-2 py-5">
+      <div id='menu-left'>
+        <img src="logo1.png" class="w-[100px]" alt="">
+      </div>
+      <div id='menu-right'>
+        <ul class="grid grid-cols-4 test-lg font-oswald text-white pt-10">
+          <li>HOME</li>
+          <li>ABOUT US</li>
+          <li>CONTACT US</li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="grid grid-cols-2">
+      <img src="brain.jpg" class="pr-8 mt-3 mb-8" alt="">
+      <div>
+        <h1 class="text-2xl text-white text-center font-lilita mt-12">STEM</h1>
+        <P class="text-white font-roboto font-light text-center">Malaysia Education Blueprint was initiated in 2011 with STEM as one of the priorities. STEM, the acronym for Science, Technology, Engineering and Mathematics is believed to be the key towards achieving a scientific and innovative society. The subjects of STEM are where students learn the skills to gather and study information (investigative skills of science), evaluate and make sense of information and last but not least, the analytical skills of mathematics.</P>
+
+        <h1 class="text-2xl text-white text-center font-lilita mt-5">SPEEDMATHS</h1>
+        <P class="text-white font-roboto font-light text-center">We aim to prepare our young generation for the future. Our SpeedMaths Programme provides students with strong foundation in Mathematics which is crucial in STEM education</P>
+      </div>
     </div>
   </div>
 </template>
