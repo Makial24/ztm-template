4bf9b74580a2e2c54b682dd121a320225bb2ad58
makial24 <you@example.com>
2022-04-05 21:37:28 +0800

    Part 2

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id='header' class="bg-[url('/hero-bg.jpg')] px-20">
+  <div>
+    <div id='header' class="bg-[url('/hero-bg.jpg')] px-20">
     <div id='menu' class="grid grid-cols-2 py-5">
       <div id='menu-left'>
         <img src="logo1.png" class="w-[100px]" alt="">
@@ -23,5 +24,16 @@
         <P class="text-white font-roboto font-light text-center">We aim to prepare our young generation for the future. Our SpeedMaths Programme provides students with strong foundation in Mathematics which is crucial in STEM education</P>
       </div>
     </div>
-  </div>
-</template>
+    </div>
+    <div class="grid grid-cols-2 bg-blue-400 py-5">
+      <div id='left'>
+        <div class="text-center">
+        <h1 class="text-3xl uppercase font-oswald pt-20">Abacus & MindMaths</h1>
+        <p class="font-roboto font-light text-grey-500 py-5 px-8">ABACUS is a tool used to solve simple mathematical problem such as addition, subtraction, multiplication, and division. Solving mathematical problem using abacus enables our children to use both their hands. Since our right brain controls the left hand and left brain controls the right hand, this promotes the use of both the left brain and the right brain simultaneously. The stimulation of both brains means learning is enhanced, better concentration on a task, builds confidence and brings out the creativity.</p>
+        <button class="font-oswald uppercase bg-red-500 text-white py-3 px-3">Explore</button>
+        </div>
+        </div>
+      <img src="abacus.png" alt="">
+    </div>
+    </div>
+    </template>
