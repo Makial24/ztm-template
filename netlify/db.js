dbc61500d52609bc97b3c03c8cdaa2ad9e05dfe1
makial24 <you@example.com>
2022-04-23 18:43:03 +0800

    Implemented DB

--- netlify/db.js
+++ netlify/db.js
@@ -5,7 +5,7 @@ const serviceAccount = JSON.parse(process.env.firebaseCredential);
 
 exports.handler = async function (event, context) {
     let app;
-    if(admin.apps.length === 0 ) {
+    if (admin.apps.length === 0 ) {
         app = initializeApp({
             credential: cert(serviceAccount),
         });
@@ -30,6 +30,6 @@ exports.handler = async function (event, context) {
             }),
         };
     } catch (error) {
-        console.log("Error adding dosument", error);
+        console.log("Error adding document", error);
     }
 }
