ed735583310e781a3db5c57a730ce0e1f5aa79c3
Makial24 <martin.makial@gmail.com>
2022-05-03 22:16:24 +0800

    Speedmaths 1.0

---
+++ pages/Locations.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <div id="header" class="md:pl-40 pl-10 pr-20 py-5">
+    <div id="menu" class="grid grid-cols-2">
+      <div id="left">
+        <img src="logo1.png" class="w-[90px]" alt="">
+      </div>
+      <div id="right">
+        <ul class="grid md:grid-cols-3 uppercase font-oswald text-lg py-5">
+          <nuxt-link to="/">Home</nuxt-link>
+          <li>Course</li>
+          <li>Locations</li>
+        </ul>
+      </div>
+    </div>
+    </div>
+    <div class="text-6xl text-center font-oswald">
+        Coming soon
+    </div>
+</div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,76 @@
 <template>
-  <div>
-    <div id='header' class="bg-[url('/hero-bg.jpg')] px-20">
-    <div id='menu' class="grid grid-cols-2 py-5">
-      <div id='menu-left'>
-        <img src="logo1.png" class="w-[100px]" alt="">
+<div>
+  <div id="header" class="md:pl-40 pl-10 pr-20 py-5">
+    <div id="menu" class="grid grid-cols-2">
+      <div id="left">
+        <img src="logo1.png" class="w-[90px]" alt="">
       </div>
-      <div id='menu-right'>
-        <ul class="grid grid-cols-4 test-lg font-oswald text-white pt-10">
-          <li>HOME</li>
-          <li>ABOUT US</li>
-          <li>CONTACT US</li>
+      <div id="right">
+        <ul class="grid md:grid-cols-3 uppercase font-oswald text-lg py-5">
+          <li>Home</li>
+          <nuxt-link to="/locations">Locations</nuxt-link>
+          <li>Course</li>
         </ul>
       </div>
     </div>
-
-    <div class="grid grid-cols-2">
-      <img src="brain.jpg" class="pr-8 mt-3 mb-8" alt="">
-      <div>
-        <h1 class="text-2xl text-white text-center font-lilita mt-12">STEM</h1>
-        <P class="text-white font-roboto font-light text-center">Malaysia Education Blueprint was initiated in 2011 with STEM as one of the priorities. STEM, the acronym for Science, Technology, Engineering and Mathematics is believed to be the key towards achieving a scientific and innovative society. The subjects of STEM are where students learn the skills to gather and study information (investigative skills of science), evaluate and make sense of information and last but not least, the analytical skills of mathematics.</P>
-
-        <h1 class="text-2xl text-white text-center font-lilita mt-5">SPEEDMATHS</h1>
-        <P class="text-white font-roboto font-light text-center">We aim to prepare our young generation for the future. Our SpeedMaths Programme provides students with strong foundation in Mathematics which is crucial in STEM education</P>
+  </div>
+  <div class="grid md:grid-cols-2 bg-blue-300 pb-10">
+    <div id="left">
+      <img src="brain.jpg" class="md:pl-20 py-10" alt="">
+    </div>
+    <div id="right" class="text-center font-oswald text-blue-900 md:px-20 px-10 md:py-10">
+      <h1 class="text-5xl">STEM</h1>
+      <p class="text-lg text-justify"> Malaysia Education Blueprint was initiated in 2022 with STEM as one of the priorities. STEM, the acronym for Science, Technology, Engineering and Mathematics is believed to be the key towards achieving a scientific and innovative society.<br>
+      The subjects of science, technology, engineering and mathematics are where students learn the skills to gather and study information (investigative skills of science), evaluate and make sense of information and last but not least, the analytical skills of mathematics. </p>
+      <p class="text-lg text-justify">The human brain consists of the right brain and the left brain. The shapes of these two parts are similar, but differences have been gradually found in their functions. The left brain is also referred to as the digital brain. It controls reading and writing, calculation, and logical thinking. The right brain is referred to as the analog brain. It controls three-dimensional sense, creativity, and artistic senses.</p>
+    </div>
+  </div>
+  <div id="section1" class="grid md:grid-cols-12 bg-green-200 py-10">
+    <div class="col-span-7 md:pl-20">
+      <img src="aboutus1.jpg" alt="">
+    </div>
+    <div class="col-span-5 md:px-20 px-10 pt-10">
+      <h1 class="text-5xl font-oswald uppercase text-center">About us</h1>
+      <p class="font-oswald text-lg text-justify">People commonly say that if you want to learn Mathematics, there is only one way to learn it i.e. by Practice because “Practice Makes Man Perfect”. Yes but practicing similar mathematical problem over and over again will cause boredom and loss of interest especially among children. Our programs are designed to solve this common problem, and on top of that, we are helping to stimulate the child’s mind with Brain Gym, and Memory Games.</p>
+      <p class="font-oswald text-lg">We are the Malaysia's leading academy in Abacus and MindMaths. We trained thousands of students since 2010.</p>
+    </div>
+  </div>
+  <div class="grid md:grid-cols-2 py-10">
+    <div class="pb-10">
+      <h1 class="text-4xl font-oswald uppercase text-center">Junior Level</h1>
+      <img src="abacus.png" class="md:px-20" alt="">
+      <p class="text-lg font-oswald md:px-10 px-5 text-center">Junior Level is specially designed for kids betweenn 6 years to 8 years old.</p>
+      <p class="text-lg font-oswald md:px-10 px-5 text-center">Kids are taught to solve mathematical problems in addition and subtraction.</p>
+    </div>
+    <div>
+      <h1 class="text-4xl font-oswald uppercase text-center">Senior Level</h1>
+      <img src="vedic.png" class="md:px-20" alt="">
+      <p class="test-lg font-oswald md:px-10 px-5 text-center">Senior Level is for pupil above the age of 11. Kids are taught to solve mathematical problems in multiplication, division, addition and subtraction</p>
+    </div>
+  </div>
+  <div id="Footer">
+    <div class="grid md:grid-cols-12 bg-yellow-200 py-10">
+      <div class="col-span-2 md:pl-40 pl-20">
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-10 w-10 stroke-1"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+        </svg>
+        <div>
+          <a href="https://api.whatsapp.com/send/?phone=60162687192"><img src="icons.png" alt=""></a>
+        </div>
+      </div>
+      <div class="col-span-4">
+        <h2 class="text-2xl font-oswald pt-5">016-268 7192</h2>
+      </div>
+      <div class="col-span-6 pl-20">
+        <a href="https://www.facebook.com/petterrayen.gnanapragasam"><img src="facebook2.png" alt=""></a>
       </div>
     </div>
-    </div>
-    <div class="grid grid-cols-2 bg-blue-400 py-5">
-      <div id='left'>
-        <div class="text-center">
-        <h1 class="text-3xl uppercase font-oswald pt-20">Abacus & MindMaths</h1>
-        <p class="font-roboto font-light text-grey-500 py-5 px-8">ABACUS is a tool used to solve simple mathematical problem such as addition, subtraction, multiplication, and division. Solving mathematical problem using abacus enables our children to use both their hands. Since our right brain controls the left hand and left brain controls the right hand, this promotes the use of both the left brain and the right brain simultaneously. The stimulation of both brains means learning is enhanced, better concentration on a task, builds confidence and brings out the creativity.</p>
-        <button class="font-oswald uppercase bg-red-500 text-white py-3 px-3 rounded rounded-lg">Explore</button>
-        </div>
-        </div>
-      <img src="abacus.png" alt="">
-    </div>
-    <div class="grid grid-cols-2 bg-green-400 pl-10 py-5">
-      <img src="vedic.png" alt="">
-      <div id='left-item-column'>
-        <h1 class="text-3xl uppercase font-oswald pt-20 text-center">Vedic Maths</h1>
-        <p class="font-roboto pt-5 pr-10 text-center">
-          VEDIC MATHS has been a proven course to perfom better in Competitive examinations. These formulae describe the way the mind naturally works and are therefore a great help in directing the student to the appropriate method of solution.
-        </p>
-      </div>
-    </div>
-    </div>
-    </template>
+  </div>
+</div>
+</template>
